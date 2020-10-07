79960cef9946284b833807ade6bc10b6371fa5b9
Manuel Zanutelli <mclzc89@gmail.com>
2020-10-07 18:54:54 -0500

    fix filter label

--- upwork-downloader-ui/src/components/Filter.jsx
+++ upwork-downloader-ui/src/components/Filter.jsx
@@ -4,9 +4,11 @@ import Checkbox from "react-simple-checkbox";
 const Filter = ({ classes, onToggleFilter }) => (
   <div className="p-4">
     <div className="border-b">
-      <div className="uppercase text-gray-500 font-bold">Label</div>
+      <div className="uppercase text-gray-500 font-bold">
+        Filter by class label
+      </div>
       <div className="flex pt-2 pb-4 justify-center">
-        {classes.map(filter => (
+        {classes.map((filter) => (
           <div key={filter.id} className="flex items-baseline mr-4 last:mr-0">
             <div data-testid={filter.id}>
               <Checkbox
